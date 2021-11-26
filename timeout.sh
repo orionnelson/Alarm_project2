dda710e9dd09220553abcec1b5073bbc1c463479
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-26 13:59:53 -0500

    Update timeout.sh

--- timeout.sh
+++ timeout.sh
@@ -2,13 +2,9 @@
 set -e
 printf "running 2"
 #sleep 32
-
-
-
 #echo `cat $1.out`
 printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
 printf "\nLooking For Expected Output :\n  $(<$1.exp)\n"
 printf "\nRecived Output :\n $(<$1.out)\n"
-
 # Commented Out until Later Stages
 #diff -q $1.exp $1.out
