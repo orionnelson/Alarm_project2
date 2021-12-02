44ae3e4a796d88d7f1c9059d5412211ab12fafde
dom-afk <67840155+dom-afk@users.noreply.github.com>
2021-12-02 17:46:41 -0500

    increasing timeout to see if it fixes test issues

--- test-alarm.sh
+++ test-alarm.sh
@@ -7,5 +7,5 @@ readarray inputs < $1.test
 rm -f $1.out
 #printf "$1.out is the test being run"
 sleep 1
-unbuffer timeout -s SIGINT 60s ./My_Alarm "${inputs[@]}" >> $1.out
+unbuffer timeout -s SIGINT 80s ./My_Alarm "${inputs[@]}" >> $1.out
 # I HATE SHELL this line shoud work but it doesnt 
